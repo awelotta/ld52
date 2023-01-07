efc4c3ed1a0582ae442893e15bf467288696c4a5
Ben Ch <benjjaa.skiiber@gmail.com>
2023-01-06 21:33:41 -0600

    Added many placeholder things for the game

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import cocos
-import menu
+import pyglet
+
+from constants import W_LEN
+import play
+
+class MenuLayer(cocos.menu.Menu):
+    def __init__(self):
+        super(MenuLayer, self).__init__()
+
+        choices = []
+        choices.append(cocos.menu.MenuItem('Play', self.on_new_game))
+        choices.append(cocos.menu.MenuItem('Quit', self.on_quit))
+        self.create_menu(choices, cocos.menu.zoom_in(), cocos.menu.zoom_out())
+
+    def on_new_game(self):
+        cocos.director.director.push(cocos.scene.Scene(play.PlayLayer()))
+
+    def on_quit(self):
+        raise SystemExit # exits
 
 class StartingLayer(cocos.layer.Layer):
     def __init__(self):
         super(StartingLayer, self).__init__()
-        
-        self.bg_layer = menu.BGLayer()
-        self.menu = menu.MenuLayer()
 
-        self.add(self.bg_layer)
+        self.bg_sprite = cocos.sprite.Sprite('resources/w_pink.jpg', position=(W_LEN*0.5, W_LEN*0.5))
+        self.bg_sprite.scale = W_LEN / max(self.bg_sprite.width, self.bg_sprite.height) # chooses the one that DOESN'T fill the whole window
+        self.add(self.bg_sprite)
+
+        self.menu = MenuLayer()
         self.add(self.menu)
         
-cocos.director.director.init(height=1200, width=1200, resizable=True)
+cocos.director.director.init(height=W_LEN, width=W_LEN, resizable=True)
 cocos.director.director.run(cocos.scene.Scene(StartingLayer()))
---
+++ toolbar.py
@@ -0,0 +1,9 @@
+import cocos
+
+from constants import W_LEN
+
+class Toolbar(cocos.layer.Layer):
+    def __init__(self):
+        super(Toolbar, self).__init__()
+
+        # blah blah
