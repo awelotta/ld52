0e030998b54bb82637e29d3c262a763d92c12e8b
save-our-sunken-ship <janey2357@outlook.com>
2023-01-08 13:09:10 -0600

    continued preliminary work

--- oldest.py
+++ oldest.py
@@ -5,15 +5,13 @@ from curses import wrapper
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_RED)
     curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_WHITE)
     BLACK_AND_RED = curses.color_pair(1)
     BLUE_AND_BLACK = curses.color_pair(2)
+    RED_AND_WHITE = curses.color_pair(3)
 #    stdscr.nodelay(True)
 
-    plot1_win = curses.newwin(3, 1, 3 ,3)
-    plot2_win = curses.newwin(7, 1, 3 ,3)
-    plot3_win = curses.newwin(3, 7, 3 ,3)
-    plot4_win = curses.newwin(7, 7, 3 ,3)
-
+    
     stdscr.clear()
     stdscr.addstr(2, 15, "HIT SPACE TO PLAY")
     stdscr.addstr(4, 15, "YOU'VE COLLECTED [THESE PLANTS]", curses.A_BOLD)
@@ -27,10 +25,42 @@ def main(stdscr):
             pass
     stdscr.addstr(8, 15, "READY", BLACK_AND_RED)
     stdscr.refresh()
+    
+
     time.sleep(1.9)
-    stdscr.border()
+
+
+
+# ^ main menu and press Space to start (part a)
+ 
+# add border color to suggest game state??
+# for instance,
+    # stdscr.attron(RED_AND_WHITE)
+    # stdscr.border()
+    # stdscr.refresh()
+    # ..
+    # stdscr.attroff(RED_AND_WHITE)
+
+
+    plot1_win = curses.newwin(3, 3, 3, 2)
+    plot2_win = curses.newwin(3, 3, 3, 9)
+    plot3_win = curses.newwin(3, 3, 8, 2)
+    plot4_win = curses.newwin(3, 3, 8, 9)
+    instruction_win = curses.newwin(20, 3, 50, 5)
+    
+
 
 wrapper(main)
+
+
+
+
+
+
+
+
+
+
 
 
 
